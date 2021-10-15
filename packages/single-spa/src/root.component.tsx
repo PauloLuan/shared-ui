c30ba5cd57c9c91c7ca3870ada79129276f0ad2e
Paulo Luan <pauloluan.inova@gmail.com>
2021-10-15 05:23:52 -0300

    ✨ feat: added styled components sample to the single spa config

--- packages/single-spa/src/root.component.tsx
+++ packages/single-spa/src/root.component.tsx
@@ -1,3 +1,18 @@
+import styled from 'styled-components'
+
+const Teste = styled.div`
+  background: blue;
+  height: 30px;
+  width: 30px;
+`
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>
+  return (
+    <>
+      <section>
+        {props.name} Essa bagaceira velha funciona com styled components
+      </section>
+      <Teste />
+    </>
+  )
 }
